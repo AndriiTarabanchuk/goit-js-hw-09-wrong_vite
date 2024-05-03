15b5f92526fb44df98bb753439906a13e7256323
AndreyTara <explorituse@gmail.com>
2024-05-03 19:52:29 +0300

    'adds-dist'

--- src/js/3-header.js
+++ src/js/3-header.js
@@ -1,19 +1,23 @@
-const gallery = document.querySelector('#gallery');
-const form = document.querySelector('#form');
-const header = document.querySelector('#header');
+export function getHeader() {
+  const gallery = document.querySelector('#gallery');
+  const form = document.querySelector('#form');
+  const header = document.querySelector('#header');
+  const strGallery = 'gallery';
+  const strForm = 'form';
 
-header.addEventListener('click', hendlerHeader);
-// console.log(header);
-export function hendlerHeader(event) {
-  event.preventDefault();
-  const currentLI = event.target.closest('.nav-item');
+  header.addEventListener('click', hendlerHeader);
+  // console.log(header);
+  function hendlerHeader(event) {
+    event.preventDefault();
+    const currentLI = event.target.closest('.nav-item');
 
-  if (currentLI.dataset.id === 'gallery') {
-    gallery.hidden = false;
-    form.hidden = true;
-  }
-  if (currentLI.dataset.id === 'form') {
-    form.hidden = false;
-    gallery.hidden = true;
+    if (currentLI.dataset.id === strGallery) {
+      gallery.hidden = false;
+      form.hidden = true;
+    }
+    if (currentLI.dataset.id === strForm) {
+      form.hidden = false;
+      gallery.hidden = true;
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,63 +1,8 @@
-import { hendlerHeader } from './js/3-header.js';
+import { getHeader } from './js/3-header.js';
+getHeader();
 
-const gallery = document.querySelector('#gallery');
-const form = document.querySelector('#form');
-const header = document.querySelector('#header');
+import { getGallery } from './js/1-gallery.js';
+getGallery();
 
-header.addEventListener('click', hendlerHeader);
-
-// Описаний в документації
-import { images } from './js/const.js';
-
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-import { createGalleryMarkup } from './js/1-gallery.js';
-
-// Отримуємо посилання на елементи DOM
-const domGallery = document.querySelector('.gallery');
-console.log(domGallery);
-
-// додаю змінну для отримання шаблонних строк
-const addGalleryMarkup = createGalleryMarkup(images);
-
-// додаємо шаблонну строку addGalleryMarkup до DOM - ".gallery"
-domGallery.insertAdjacentHTML('beforeend', addGalleryMarkup);
-
-// use library SimpleLightbox
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-  overlayOpacity: 0.8,
-  widthRatio: 0.77,
-  heightRatio: 0.919,
-});
-
-import { resetForm, onInputData, hendlerSubmit } from './js/2-form.js';
-//инпорт лода
-import throttle from 'lodash.throttle';
-
-// визначили сталу змінну ключ для локалстораджу
-const LOCAL_KEY = 'feedback-form-state';
-
-// визначили Дом елемент для форми
-const formDom = document.querySelector('.feedback-form');
-
-// деструктурізація DOM
-const { email, message } = formDom.elements;
-
-//отримання останніх данних з локалстореджа або
-let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
-
-// очищення значень у полях ДОМ Form
-resetForm(formData, formDom);
-
-// слухач на зміну значень  email, message
-formDom.addEventListener('input', throttle(onInputData, 500));
-
-// слухач на submit
-formDom.addEventListener('submit', hendlerSubmit);
+import { analisForm } from './js/2-form.js';
+analisForm();
